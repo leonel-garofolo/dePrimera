5837b0b601f4cf64ccb55af7e36186abbda7de02
leonel@sima.ag <leonel@sima.ag>
2020-07-02 18:04:18 -0300

    gcloud yaml

--- api/main.go
+++ api/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"deprimera/api/router"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigFile("../app.yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	miMain := viper.Get("test")
+	log.Println(miMain)
+
+	value := viper.Get("database.url")
+	log.Println(value)
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/gorm v1.9.14
 	github.com/labstack/echo/v4 v4.1.16
+	github.com/spf13/viper v1.7.0
 )
